013d1a54db0f2f83807450615979bc34adb17c5d
yaoguangfiles <yaoguangfiles@gmail.com>
2023-03-25 23:16:55 -0400

    work. chatGpt_api02/ works to get answer for the question 'what is your name?' to chat GPT

---
+++ chatGpt_api01/chatGpt_api01/chatGpt_api01/Program.cs
@@ -0,0 +1,83 @@
+﻿
+//from: https://michaelceber.medium.com/chat-bot-using-chat-gpt-3-5-in-c-bb9c9a21f7db
+
+// See: https://platform.openai.com/docs/guides/chat/introduction
+// See: https://platform.openai.com/docs/api-reference/chat
+
+using Newtonsoft.Json;
+using Spectre.Console;
+
+namespace chatGpt_api01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting chat ...");
+
+            do_chatAsync();
+        }
+
+        static async Task do_chatAsync()
+        {
+            const string key = "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";
+            const string url = "https://api.openai.com/v1/chat/completions";
+
+            // Initialise the chat by describing the assistant,
+            // and providing the assistants first question to the user
+            var messages = new List<dynamic>
+            {
+                new {role = "system",
+                    content = "You are ChatGPT, a large language " +
+                                                "model trained by OpenAI. " +
+                                                "Answer as concisely as possible.  " +
+                                                "Make a joke every few lines just to spice things up."},
+                new {role = "assistant",
+                    content = "How can I help you?"}
+            };
+            AnsiConsole.MarkupLine($"[purple]MACHINE:[/] [blue]{Markup.Escape(messages[1].content)}[/]");
+
+            while (true)
+            {
+                // Capture the users messages and add to
+                // messages list for submitting to the chat API
+                var userMessage = AnsiConsole.Ask<string>($"[purple]USER:[/] ");
+                messages.Add(new { role = "user", content = userMessage });
+
+                // Create the request for the API sending the
+                // latest collection of chat messages
+                var request = new
+                {
+                    messages,
+                    model = "gpt-3.5-turbo",
+                    max_tokens = 300,
+                };
+
+                // Send the request and capture the response
+                var httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {key}");
+                var requestJson = JsonConvert.SerializeObject(request);
+                var requestContent = new StringContent(requestJson, System.Text.Encoding.UTF8, "application/json");
+                var httpResponseMessage = await httpClient.PostAsync(url, requestContent);
+                var jsonString = await httpResponseMessage.Content.ReadAsStringAsync();
+                var responseObject = JsonConvert.DeserializeAnonymousType(jsonString, new
+                {
+                    choices = new[] { new { message = new { role = string.Empty, content = string.Empty } } },
+                    error = new { message = string.Empty }
+                });
+
+
+                if (!string.IsNullOrEmpty(responseObject?.error?.message))  // Check for errors
+                {
+                    AnsiConsole.MarkupLine($"[bold red]{Markup.Escape(responseObject?.error.message)}[/]");
+                }
+                else  // Add the message object to the message collection
+                {
+                    var messageObject = responseObject?.choices[0].message;
+                    messages.Add(messageObject);
+                    AnsiConsole.MarkupLine($"[purple]MACHINE:[/] [blue]{Markup.Escape(messageObject.content)}[/]");
+                }
+            }
+        }
+    }
+}
---
+++ chatGpt_api02/chatGpt_api02/chatGpt_api02/Program.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace chatGpt_api02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting a question ...");
+
+            Task<string> answer_task = Get_answer();
+            string answer = answer_task.Result;
+
+            int t = 0;
+
+        }
+
+
+        public static async Task<string> Get_answer()
+        {
+            string bearerToken = "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";
+
+            //string url_request_base = "https://gif-apim.pwcinternal.com/pwclabs-catalogue-us-sandbox/api/v2/workspaces";
+            //string workspace_id = workspaceId.ToString();
+
+            //string url_request = $"{url_request_base}/{workspace_id}/assets?subscription-key={subscription_key}";
+            string url_request = "https://api.openai.com/v1/chat/completions";
+
+
+            string body_register_temp = @"
+            {
+                ""model"" : ""gpt-3.5-turbo"",
+                ""messages"" : [{""role"": ""user"", ""content"": ""what is your name?""}]
+            }
+            ";
+
+
+            var req_register = new HttpRequestMessage(HttpMethod.Post, url_request);
+            req_register.Headers.Add("Authorization", "Bearer " + bearerToken); // bearer authentication
+
+            //req_register.Headers.Add("Content-Type", "application/json");
+            req_register.Headers.Add("Accept", "*/*");
+            req_register.Headers.Add("Accept-Encoding", "gzip, deflate, br");
+
+            string body_register = body_register_temp;
+            req_register.Content = new StringContent(body_register,
+                                    Encoding.UTF8,
+                                    "application/json");//CONTENT-TYPE header
+
+            var httpClient_register = new HttpClient();
+
+            HttpResponseMessage response = await httpClient_register.SendAsync(req_register);
+
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            //AssetResponse.Rootobject rootobject = JsonConvert.DeserializeObject<AssetResponse.Rootobject>(responseBody);
+
+            //string asset_id = rootobject.id;
+
+            return responseBody;
+        }
+
+    }
+}
